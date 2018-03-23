87ba2b773d749b82ed8c90bff0a005f2b7b80cc6
Alex Moschos <alexgmoschos@gmail.com>
2018-03-23 21:57:18 +0200

    Removed unnecessary files

--- search_ep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-VAR=$(grep -m 1 'def' $1 | tr  ' ' ' ')
-readonly VAR
-
-VAR_NO_TRAIL_SPACE="$(echo -e "${VAR}" | sed -e 's/[[:space:]]*$//')"
-
-Entry_point="$(echo $VAR_NO_TRAIL_SPACE | awk '{print $NF}')"
-readonly Entry_point
-echo ${Entry_point}
